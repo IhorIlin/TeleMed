524ae8f250b5b004bd45dfb899508e5d21215ede
Ihor Ilin <igordevmail@gmail.com>
2025-05-30 09:48:50 +0200

    add unit tests for LoginViewModel.

---
+++ TeleMedTests/Mocks/MockAuthService.swift
@@ -0,0 +1,29 @@
+//
+//  MockAuthService.swift
+//  TeleMedTests
+//
+//  Created by Ihor Ilin on 29.05.2025.
+//
+
+import Combine
+@testable import TeleMed
+
+final class MockAuthService: AuthService {
+    var loginCalled = false
+    var loginResult: Result<LoginResponseDTO, NetworkClientError> = .success(.init(token: "mock-token"))
+
+    func login(with request: LoginRequestDTO) -> AnyPublisher<LoginResponseDTO, NetworkClientError> {
+        loginCalled = true
+
+        switch loginResult {
+        case .success(let response):
+            return Just(response)
+                .setFailureType(to: NetworkClientError.self)
+                .eraseToAnyPublisher()
+
+        case .failure(let error):
+            return Fail(error: error)
+                .eraseToAnyPublisher()
+        }
+    }
+}
