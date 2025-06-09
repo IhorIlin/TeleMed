ddcb63379c7c1760a7c885c6e1cd1f38597a4485
Ihor Ilin <igordevmail@gmail.com>
2025-06-09 21:32:51 +0200

    Add SDWebImage pod.

---
+++ TeleMed/Modules/Profile/Views/Cells/ProfileTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileTableViewCell.swift
+//  TeleMed
+//
+//  Created by Ihor Ilin on 09.06.2025.
+//
+
+import UIKit
+import SnapKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+}
